8062b0e8ca761c0b310782cc324c63df5fe363d0
Pawee Kittiwathanakul <boatmind2710@gmail.com>
2019-10-02 07:50:37 +0700

    Add LuckyNumberCollectionViewController

---
+++ AccountLuckyNumber/LuckyNumberCollectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LuckyNumberCollectionViewController.swift
+//  AccountLuckyNumber
+//
+//  Created by Pawee Kittiwathanakul on 2/10/2562 BE.
+//  Copyright © 2562 bobo. All rights reserved.
+//
+
+import UIKit
+
+class LuckyNumberCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
