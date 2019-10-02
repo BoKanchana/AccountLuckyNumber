7f5fab24e962314344a697e122baa35ae03e6fe4
Pawee Kittiwathanakul <boatmind2710@gmail.com>
2019-10-02 07:59:47 +0700

    Add LuckyNumberCollectionTableViewCell

---
+++ AccountLuckyNumber/cell/LuckyNumberCollectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LuckyNumberCollectionTableViewCell.swift
+//  AccountLuckyNumber
+//
+//  Created by Pawee Kittiwathanakul on 2/10/2562 BE.
+//  Copyright © 2562 bobo. All rights reserved.
+//
+
+import UIKit
+
+class LuckyNumberCollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
