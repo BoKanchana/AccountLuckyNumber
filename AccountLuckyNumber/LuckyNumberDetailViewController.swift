0011426fec59db7d71dead8f510939a78c248a95
Kanchana Phakdeedorn <kanchana.pdk@gmail.com>
2019-10-03 00:56:07 +0700

    Add getLuckyNumberDetail function

--- AccountLuckyNumber/LuckyNumberDetailViewController.swift
+++ AccountLuckyNumber/LuckyNumberDetailViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class LuckyNumberDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var luckyNumberImage: UIImageView!
+  @IBOutlet weak var luckyNumberLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    getLuckyNumberDetail()
+  }
+  
+  func getLuckyNumberDetail() {
+    let collection = "Work"
+    let id = "9999999999"
+    FirebaseManager().getLuckyNumberDetail(collection: collection, id: id) { result in
+      self.luckyNumberLabel.text = result.accountLuckyNumber
+      self.descriptionLabel.text = result.description
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
 }
