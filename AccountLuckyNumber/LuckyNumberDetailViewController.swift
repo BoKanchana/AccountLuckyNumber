f6bf3cc900d569be7fb0ca7bb32f45f0861b0295
Piyada Suwansa-ard <swsa.piyada@gmail.com>
2019-10-03 19:59:15 +0700

    modify ui

--- AccountLuckyNumber/LuckyNumberDetailViewController.swift
+++ AccountLuckyNumber/LuckyNumberDetailViewController.swift
@@ -9,47 +9,56 @@
 import UIKit
 
 class LuckyNumberDetailViewController: UIViewController {
-  
-  @IBOutlet weak var luckyNumberImage: UIImageView!
-  @IBOutlet weak var luckyNumberLabel: UILabel!
-  @IBOutlet weak var descriptionLabel: UILabel!
-  @IBAction func confirmOrderButton(_ sender: Any) {
-    generateDeeplink()
-  }
-  var token: String = ""
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    getLuckyNumberDetail()
-    getAccessToken()
-  }
-  
-  func getAccessToken() {
-    FirebaseManager().getAccessToken() { result in
-      self.token = result as! String
+    
+    @IBOutlet weak var luckyNumberImage: UIImageView!
+    @IBOutlet weak var luckyNumberLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBAction func confirmOrderButton(_ sender: Any) {
+        generateDeeplink()
     }
-  }
-  
-  func generateDeeplink() {
-    FirebaseManager().generateDeeplink(token: token) { result in
-      let deeplink = result as! String
-      if deeplink != "" {
-       self.goToEasyApp(deeplink: deeplink)
-      }
+    var token: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getLuckyNumberDetail()
+        getAccessToken()
     }
-  }
-  
-  func goToEasyApp(deeplink: String) {
-    UIApplication.shared.openURL(NSURL(string: "\(deeplink)?callback_url=accountluckynumber://")! as URL)
-  }
-  
-  func getLuckyNumberDetail() {
-    let collection = "Work"
-    let id = "9999999999"
-    FirebaseManager().getLuckyNumberDetail(collection: collection, id: id) { result in
-      self.luckyNumberLabel.text = result.accountLuckyNumber
-      self.descriptionLabel.text = result.description
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+        navigationController?.navigationBar.backgroundColor = UIColorFromRGB(rgbValue: 0x572993)
+        navigationController?.navigationBar.barTintColor = UIColorFromRGB(rgbValue: 0x572993)
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
-  }
-  
+    
+    func getAccessToken() {
+        FirebaseManager().getAccessToken() { result in
+            self.token = result as! String
+        }
+    }
+    
+    func generateDeeplink() {
+        FirebaseManager().generateDeeplink(token: token) { result in
+            let deeplink = result as! String
+            if deeplink != "" {
+                self.goToEasyApp(deeplink: deeplink)
+            }
+        }
+    }
+    
+    func goToEasyApp(deeplink: String) {
+        UIApplication.shared.openURL(NSURL(string: "\(deeplink)?callback_url=accountluckynumber://")! as URL)
+    }
+    
+    func getLuckyNumberDetail() {
+        let collection = "Work"
+        let id = "9999999999"
+        FirebaseManager().getLuckyNumberDetail(collection: collection, id: id) { result in
+            self.luckyNumberLabel.text = result.accountLuckyNumber
+            self.descriptionLabel.text = result.description
+        }
+    }
+    
 }
